d3c70b352eb9b83601d870efbd00d93a560afa75
David Okunade <davidokunade1@gmail.com>
2020-04-29 15:02:01 +0100

    A todoApp made with React

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p> This is the version 1.0.0. of this app</p>
+    </React.Fragment>
+  );
+}
+
+export default About;
